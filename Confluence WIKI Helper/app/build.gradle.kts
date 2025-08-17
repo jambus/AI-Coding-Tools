6dc32b2b971d48f29fe5f174eef29ab508a494f1
Liang SHEN <jambus2010@gmail.com>
2025-08-17 21:45:00 +0800

    Bug fix during build

--- Confluence WIKI Helper/app/build.gradle.kts
+++ Confluence WIKI Helper/app/build.gradle.kts
@@ -33,11 +33,25 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    
+    kapt {
+        correctErrorTypes = true
+        javacOptions {
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED")
+            option("--add-opens", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED")
+        }
     }
     buildFeatures {
         compose = true
